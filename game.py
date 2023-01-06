d6f126cc75335c00d3b983fb3e2bcfb7f5a8cab6
Polaris66 <mathematician2b@gmail.com>
2023-01-07 02:34:20 +0530

    Done with game

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 
 NO_OF_SQUARES = 3
+#TODO: Implement for bigger squares
 
 Symbol = {
     0:" ",
@@ -17,6 +18,9 @@ class Game:
     def reset(self):
         self.state = np.zeros(NO_OF_SQUARES*NO_OF_SQUARES,dtype = int)
         self.turn = 1
+        self.done = False
+        self.winner = 0
+        self.count = 0
         #print(self.state)
     
     def _render(self):
@@ -30,13 +34,46 @@ class Game:
         
 
     def play_step(self,action):
-        idx = action
-        self.state[idx] = self.turn
-        self.turn*=-1
-        self._render()
+        if self.done:
+            self._render()
+            print(self.winner," won")
+            self.reset()
+        else:
+            idx = action
+            if(self.state[idx]==0):
+                self.state[idx] = self.turn
+                self.turn*=-1
+                self._render()
+                self._check_game_over()
+        
+    def _check_game_over(self):
+        self.count+=1
+        winner = 0
+        for i in range(3):
+            if (self.state[i] == self.state[i+3]) and (self.state[i+6] == self.state[i+3]):
+                winner = self.state[i] 
+        
+        for i in [0,3,6]:
+            if (self.state[i] == self.state[i+1]) and (self.state[i+2] == self.state[i+1]):
+                winner = self.state[i] 
+       
+        if (self.state[0] == self.state[4]) and (self.state[8] == self.state[4]):
+                winner = self.state[0] 
+        
+        if (self.state[2] == self.state[4]) and (self.state[6] == self.state[4]):
+                winner = self.state[2] 
+        
+        if(winner!=0):
+            self.done = True
+            self.winner = winner
+        if(self.count==9):
+            self.done = True
+            self.winner = 0
 
 if __name__ == '__main__':
     game = Game()
-    for i in range(9):
-        action = int(input())
+    while not game.done:
+        action = int(input()) # Replace with Model
         game.play_step(action)
+    game._render()
+    print(Symbol[game.winner],"Won")
